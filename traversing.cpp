87eff71d0672f3a05314274ca559fd213f6a8782
Aashish203 <80540291+Aashish203@users.noreply.github.com>
2021-10-01 23:47:08 +0530

    traversing linkedlist

---
+++ traversing.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+	int data;
+	node *next;
+	node(int x)
+	{
+		data=x;
+		next=NULL;
+	}
+};
+
+void print(node *head)
+{
+	node *curr=head;
+	while(curr!=NULL)
+	{
+		cout<<curr->data;
+		curr=curr->next;
+	}
+}
+
+int main()
+{
+	node *head=new node(10);
+	node *temp1=new node(20);
+	node *temp2=new node(30);
+	head->next=temp1;
+	temp1->next=temp2;
+	print(head);
+	return 0;
+}
